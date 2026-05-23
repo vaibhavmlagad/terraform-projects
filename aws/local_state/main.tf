768a05901f18a74360a0722cdcb3aa0d584e68e5
Vaibhav Lagad <vaibhav.lagad12@gmail.com>
2026-05-23 18:02:10 +0530

    main.tf added in local state

---
+++ aws/local_state/main.tf
@@ -0,0 +1,24 @@
+terraform {
+
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 4.16"
+        }
+    }
+
+    required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "my_demo_server" {
+    ami           = "ami-07a00cf47dbbc844c"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "TerraformExampleInstance"
+    }
+}
